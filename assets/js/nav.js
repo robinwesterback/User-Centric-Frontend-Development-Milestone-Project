aa25113d4887d5cb9aeb63dc9d1e5892b02f2d40
Robin Westerback <robinwesterback1989@gmail.com>
2020-05-28 16:12:19 +0000

    Fix contact section CSS for relevant media queries

---
+++ assets/js/nav.js
@@ -0,0 +1,9 @@
+   function openNav() {
+          document.getElementById("myNav").style.height = "100%";
+          document.getElementById("logo").style.color = "#ffffff";
+        }
+
+        function closeNav() {
+          document.getElementById("myNav").style.height = "0%";
+          document.getElementById("logo").style.color = "#1d1d1d";
+        }
